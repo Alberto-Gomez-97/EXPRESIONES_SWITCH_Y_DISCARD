e2652ed146fd1617ee13177db7ee98558d82f0fb
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-08 16:37:50 -0600

    Add files via upload

---
+++ Expresiones_switch_y_Discard_udy/Expresiones_switch_y_Discard_udy/Program.cs
@@ -0,0 +1,15 @@
+﻿//EXPRESIONES SWITCH Y DISCARD
+
+// se abrebian switch con una expresion de switch
+
+var lugar = 1;
+var mensaje = lugar switch
+{
+    1 => "Quedaste en primer lugar",
+    2 => "Quedast en segundo lugar",
+    3 => "Quedaste en tercer lugar",
+    4 or 5 => "Casi lo logras",
+    //DISCARD
+    _ => "Mejor suerte la proxima"
+};
+Console.WriteLine(mensaje);
